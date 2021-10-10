6407cd6ac6f7d806c444e26b538d122ef0611ec5
Sebastian Young Ye <yye3028@gmail.com>
2021-10-11 02:37:40 +1100

    Cleaned up settings

--- MapMod/Settings/LocalSettings.cs
+++ MapMod/Settings/LocalSettings.cs
@@ -49,25 +49,7 @@ namespace VanillaMapMod.Settings
             {
 				entry.Value.Has = true;
             }
-
-            //HasSkillPin = true;
-            //HasCharmPin = true;
-            //HasKeyPin = true;
-            //HasMaskPin = true;
-            //HasVesselPin = true;
-            //HasNotchPin = true;
-            //HasOrePin = true;
-            //HasEggPin = true;
-            //HasRelicPin = true;
-            //HasEssenceBossPin = true;
-            //HasCocoonPin = true;
-            //HasGeoPin = true;
-            //HasRockPin = true;
-            //HasTotemPin = true;
-            //HasLorePin = true;
         }
-
-        
 
 		public bool GetHasFromGroup(string group)
 		{
@@ -77,43 +59,7 @@ namespace VanillaMapMod.Settings
             }
 
 			return false;
-
-			//return group switch
-			//{
-			//	"Skill" => HasSkillPin,
-			//	"Charm" => HasCharmPin,
-			//	"Key" => HasKeyPin,
-			//	"Mask" => HasMaskPin,
-			//	"Vessel" => HasVesselPin,
-			//	"Notch" => HasNotchPin,
-			//	"Ore" => HasOrePin,
-			//	"Egg" => HasEggPin,
-			//	"Relic" => HasRelicPin,
-			//	"EssenceBoss" => HasEssenceBossPin,
-			//	"Cocoon" => HasCocoonPin,
-			//	"Geo" => HasGeoPin,
-			//	"Rock" => HasRockPin,
-			//	"Totem" => HasTotemPin,
-			//	"Lore" => HasLorePin,
-			//	_ => false,
-			//};
 		}
-
-		//public bool HasSkillPin = false;
-		//      public bool HasCharmPin = false;
-		//      public bool HasKeyPin = false;
-		//      public bool HasMaskPin = false;
-		//      public bool HasVesselPin = false;
-		//      public bool HasNotchPin = false;
-		//      public bool HasOrePin = false;
-		//      public bool HasEggPin = false;
-		//      public bool HasRelicPin = false;
-		//      public bool HasEssenceBossPin = false;
-		//      public bool HasCocoonPin = false;
-		//      public bool HasGeoPin = false;
-		//      public bool HasRockPin = false;
-		//      public bool HasTotemPin = false;
-		//      public bool HasLorePin = false;
 
 		public void SetHasFromGroup(string group, bool value)
 		{
@@ -131,26 +77,6 @@ namespace VanillaMapMod.Settings
 			}
 
 			return false;
-
-			//return group switch
-			//{
-			//	"Skill" => SkillOn,
-			//	"Charm" => CharmOn,
-			//	"Key" => KeyOn,
-			//	"Mask" => MaskOn,
-			//	"Vessel" => VesselOn,
-			//	"Notch" => NotchOn,
-			//	"Ore" => OreOn,
-			//	"Egg" => EggOn,
-			//	"Relic" => RelicOn,
-			//	"EssenceBoss" => EssenceBossOn,
-			//	"Cocoon" => CocoonOn,
-			//	"Geo" => GeoOn,
-			//	"Rock" => RockOn,
-			//	"Totem" => RockOn,
-			//	"Lore" => LoreOn,
-			//	_ => false,
-			//};
 		}
 
 		public void SetOnFromGroup(string group, bool value)
@@ -159,85 +85,6 @@ namespace VanillaMapMod.Settings
 			{
 				GroupSettings[group].On = value;
 			}
-
-			//switch (group)
-			//{
-			//	case "Skill":
-			//		SkillOn = value;
-			//		break;
-
-			//	case "Charm":
-			//		CharmOn = value;
-			//		break;
-
-			//	case "Key":
-			//		KeyOn = value;
-			//		break;
-
-			//	case "Mask":
-			//		MaskOn = value;
-			//		break;
-
-			//	case "Vessel":
-			//		VesselOn = value;
-			//		break;
-
-			//	case "Notch":
-			//		NotchOn = value;
-			//		break;
-
-			//	case "Ore":
-			//		OreOn = value;
-			//		break;
-
-			//	case "Egg":
-			//		EggOn = value;
-			//		break;
-
-			//	case "Relic":
-			//		RelicOn = value;
-			//		break;
-
-			//	case "EssenceBoss":
-			//		EssenceBossOn = value;
-			//		break;
-
-			//	case "Cocoon":
-			//		CocoonOn = value;
-			//		break;
-
-			//	case "Geo":
-			//		GeoOn = value;
-			//		break;
-
-			//	case "Rock":
-			//		RockOn = value;
-			//		break;
-
-			//	case "Totem":
-			//		TotemOn = value;
-			//		break;
-
-			//	case "Lore":
-			//		LoreOn = value;
-			//		break;
-			//}
 		}
-
-		//public bool SkillOn = true;
-		//public bool CharmOn = true;
-		//public bool KeyOn = true;
-		//public bool MaskOn = true;
-		//public bool VesselOn = true;
-		//public bool NotchOn = true;
-		//public bool OreOn = true;
-		//public bool EggOn = true;
-		//public bool RelicOn = true;
-		//public bool EssenceBossOn = true;
-		//public bool CocoonOn = true;
-		//public bool GeoOn = true;
-		//public bool RockOn = true;
-		//public bool TotemOn = true;
-		//public bool LoreOn = true;
 	}
 }
--- MapMod/Settings/SettingsUtil.cs
+++ MapMod/Settings/SettingsUtil.cs
@@ -9,7 +9,6 @@ namespace VanillaMapMod.Settings
 {
     public static class SettingsUtil
     {
-		// TODO: Clean up this mess
 		public static bool GetPlayerDataMapSetting(string mapArea)
 		{
 			return mapArea switch
@@ -31,52 +30,6 @@ namespace VanillaMapMod.Settings
 				_ => false,
 			};
 		}
-
-		//public static bool GetMapModSettingFromPool(string pool)
-		//{
-		//	return pool switch
-		//	{
-		//		"Skill" => VanillaMapMod.LS.HasSkillPin,
-		//		"Charm" => VanillaMapMod.LS.HasCharmPin,
-		//		"Key" => VanillaMapMod.LS.HasKeyPin,
-		//		"Mask" => VanillaMapMod.LS.HasMaskPin,
-		//		"Vessel" => VanillaMapMod.LS.HasVesselPin,
-		//		"Notch" => VanillaMapMod.LS.HasNotchPin,
-		//		"Ore" => VanillaMapMod.LS.HasOrePin,
-		//		"Egg" => VanillaMapMod.LS.HasEggPin,
-		//		"Relic" => VanillaMapMod.LS.HasRelicPin,
-		//		"EssenceBoss" => VanillaMapMod.LS.HasEssenceBossPin,
-		//		"Cocoon" => VanillaMapMod.LS.HasCocoonPin,
-		//		"Geo" => VanillaMapMod.LS.HasGeoPin,
-		//		"Rock" => VanillaMapMod.LS.HasRockPin,
-		//		"Totem" => VanillaMapMod.LS.HasTotemPin,
-		//		"Lore" => VanillaMapMod.LS.HasLorePin,
-		//		_ => true,
-		//	};
-		//}
-
-		//public static bool GetMapModSettingFromBoolName(string pool)
-		//{
-		//	return pool switch
-		//	{
-		//		"HasSkillPin" => VanillaMapMod.LS.HasSkillPin,
-		//		"HasCharmPin" => VanillaMapMod.LS.HasCharmPin,
-		//		"HasKeyPin" => VanillaMapMod.LS.HasKeyPin,
-		//		"HasMaskPin" => VanillaMapMod.LS.HasMaskPin,
-		//		"HasVesselPin" => VanillaMapMod.LS.HasVesselPin,
-		//		"HasNotchPin" => VanillaMapMod.LS.HasNotchPin,
-		//		"HasOrePin" => VanillaMapMod.LS.HasOrePin,
-		//		"HasEggPin" => VanillaMapMod.LS.HasEggPin,
-		//		"HasRelicPin" => VanillaMapMod.LS.HasRelicPin,
-		//		"HasEssenceBossPin" => VanillaMapMod.LS.HasEssenceBossPin,
-		//		"HasPinCocoon" => VanillaMapMod.LS.HasCocoonPin,
-		//		"HasGeoPin" => VanillaMapMod.LS.HasGeoPin,
-		//		"HasRockPin" => VanillaMapMod.LS.HasRockPin,
-		//		"HasTotemPin" => VanillaMapMod.LS.HasTotemPin,
-		//		"HasLorePin" => VanillaMapMod.LS.HasLorePin,
-		//		_ => true,
-		//	};
-		//}
 
 		public static bool IsMapModSetting(string boolName)
 		{
@@ -101,60 +54,5 @@ namespace VanillaMapMod.Settings
             };
         }
 
-		//public static void SetMapModSetting(string boolName, bool value)
-		//{
-		//	switch (boolName)
-  //          {
-		//		case "HasSkillPin":
-		//			VanillaMapMod.LS.HasSkillPin = value;
-		//			break;
-		//		case "HasCharmPin":
-		//			VanillaMapMod.LS.HasCharmPin = value;
-		//			break;
-		//		case "HasKeyPin":
-		//			VanillaMapMod.LS.HasKeyPin = value;
-		//			break;
-		//		case "HasMaskPin":
-		//			VanillaMapMod.LS.HasMaskPin = value;
-		//			break;
-		//		case "HasVesselPin":
-		//			VanillaMapMod.LS.HasVesselPin = value;
-		//			break;
-		//		case "HasNotchPin":
-		//			VanillaMapMod.LS.HasNotchPin = value;
-		//			break;
-		//		case "HasOrePin":
-		//			VanillaMapMod.LS.HasOrePin = value;
-		//			break;
-		//		case "HasEggPin":
-		//			VanillaMapMod.LS.HasEggPin = value;
-		//			break;
-		//		case "HasRelicPin":
-		//			VanillaMapMod.LS.HasRelicPin = value;
-		//			break;
-		//		case "HasEssenceBossPin":
-		//			VanillaMapMod.LS.HasEssenceBossPin = value;
-		//			break;
-		//		// Overwrite vanilla behaviour of cocoon pins
-		//		case "hasPinCocoon":
-		//			VanillaMapMod.LS.HasCocoonPin = value;
-		//			PlayerData.instance.hasPinCocoon = value;
-		//			break;
-		//		case "HasGeoPin":
-		//			VanillaMapMod.LS.HasGeoPin = value;
-		//			break;
-		//		case "HasRockPin":
-		//			VanillaMapMod.LS.HasRockPin = value;
-		//			break;
-		//		case "HasTotemPin":
-		//			VanillaMapMod.LS.HasTotemPin = value;
-		//			break;
-		//		case "HasLorePin":
-		//			VanillaMapMod.LS.HasLorePin = value;
-		//			break;
-		//		default:
-		//			break;
-		//	}
-		//}
 	}
 }
