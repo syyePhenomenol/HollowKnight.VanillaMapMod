d533e2fcd82fe5a31eeeb73301247d7ee6b4e1ea
Sebastian Young Ye <yye3028@gmail.com>
2021-11-16 08:49:09 +1100

    Fix GUI unload

--- MapMod/UI/GUI.cs
+++ MapMod/UI/GUI.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections;
 using Modding;
 using UnityEngine.SceneManagement;
 
@@ -9,10 +9,11 @@ namespace VanillaMapMod.UI
         public static void Hook()
         {
             On.GameMap.Start += GameMap_Start;
-            UnityEngine.SceneManagement.SceneManager.activeSceneChanged += HandleSceneChanges;
+            On.QuitToMenu.Start += OnQuitToMenu;
+            //UnityEngine.SceneManagement.SceneManager.activeSceneChanged += HandleSceneChanges;
         }
 
-        public static void GameMap_Start(On.GameMap.orig_Start orig, GameMap self)
+        private static void GameMap_Start(On.GameMap.orig_Start orig, GameMap self)
         {
             orig(self);
 
@@ -20,12 +21,19 @@ namespace VanillaMapMod.UI
             GUIController.Instance.BuildMenus();
         }
 
-        private static void HandleSceneChanges(Scene from, Scene to)
+        private static IEnumerator OnQuitToMenu(On.QuitToMenu.orig_Start orig, QuitToMenu self)
         {
-            if (to.name == "Quit_to_Menu")
-            {
-                GUIController.Unload();
-            }
+            GUIController.Unload();
+
+            return orig(self);
         }
+
+        //private static void HandleSceneChanges(Scene from, Scene to)
+        //{
+        //    if (to.name == "Quit_to_Menu")
+        //    {
+        //        GUIController.Unload();
+        //    }
+        //}
     }
 }
